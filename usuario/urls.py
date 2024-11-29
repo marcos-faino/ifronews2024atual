9acf396db0f2beadff8e8572ef30aa721efb3f4d
Marcos Alves Faino <1812293@ifro.local>
2024-11-28 22:13:56 -0400

    login e autenticação de usuários

--- usuario/urls.py
+++ usuario/urls.py
@@ -5,4 +5,6 @@ from usuario import views
 urlpatterns = [
     path('cadastro/', views.UsuarioCreateView.as_view(),
          name='cadusuario'),
+    path('login/', views.LoginUserView.as_view(), name='loginuser'),
+    path('logout/', views.LogoutUserView.as_view(), name='logoutuser'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,7 +1,12 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView, FormView
 
 from usuario.forms import UsuarioCreationForm
 
@@ -22,3 +27,31 @@ class UsuarioCreateView(CreateView):
     def form_invalid(self, form):
         messages.error(self.request, "Usuário não pôde ser cadastrado!")
         return super().form_invalid(form)
+
+
+class LoginUserView(FormView):
+    template_name = 'usuario/login.html'
+    model = User
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.cleaned_data['username']
+        senha = form.cleaned_data['password']
+        usuario = authenticate(self.request, username=user, password=senha)
+        if usuario is not None:
+            login(self.request, usuario)
+        messages.error(self.request, "Usuário ou senha inválida")
+        return super().form_valid(form)
+
+
+    def form_invalid(self, form):
+        messages.error(self.request, "Usuário ou senha inválida")
+        return super().form_invalid(form)
+
+
+class LogoutUserView(LoginRequiredMixin, LogoutView):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('home')
